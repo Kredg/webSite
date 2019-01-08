b654b80996a772e3931dda0d46f4167a35ecca06
Kredg <45512263+Kredg@users.noreply.github.com>
2019-01-08 21:53:49 +0100

    codeigniter

---
+++ application/views/cv_titre.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Bourbasquet Kévin | CV</title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Curriculum Vitae</h1>
+
+	<div id="body">
+		<p>teeeeest</p>
+
+		<p>Test ??</p>
+		<code>application/views/jsp_message.php</code>
+
+		<p>ça marche !</p>
+		<code>alllezzzzzz!!!</code>
+
+		<p>un deux un deux <a href="123/">123</a>.</p>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+</div>
+</body>
+</html>
